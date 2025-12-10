b2f13ce3bc82d9e9f9e68edb4e853af569583fb7
RayMaroun <raymondmaroun15@gmail.com>
2025-12-10 20:03:46 +0200

    Update workbook 9

---
+++ pluralsight/java-development/workbook-9/ledgerapi4/src/main/java/com/pluralsight/ledgerapi4/models/Transaction.java
@@ -0,0 +1,46 @@
+package com.pluralsight.ledgerapi4.models;
+
+public class Transaction {
+    private Integer transactionId;
+    private double amount;
+    private String vendor;
+
+    public Transaction(Integer transactionId, double amount, String vendor) {
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.vendor = vendor;
+    }
+
+    public Integer getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Integer transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", amount=" + amount +
+                ", vendor='" + vendor + '\'' +
+                '}';
+    }
+}
